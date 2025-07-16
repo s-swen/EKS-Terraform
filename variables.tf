8bad960985f63e39589e220c367bdbb1731802fb
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2025-07-16 23:10:36 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,5 @@
 variable "ssh_key_name" {
-  description = "The name of the SSH key pair to use for instances"
+  description = "The name of the SSH key pair to use for EC2 instances in the EKS node group"
   type        = string
-  default     = "DevOps-Shack"
+  default     = "sadie"
 }
-
