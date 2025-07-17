8c253341daca2fba1e076f77d2ea8190fa129087
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2025-07-17 17:33:15 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,36 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_vpc" "selected" {
+# Reference existing VPC
+data "aws_vpc" "eks_vpc" {
   id = "vpc-04e673c1da2ee7b78"
 }
 
-data "aws_subnet" "selected" {
+# Reference 2 subnets in different AZs
+data "aws_subnet" "subnet1" {
   id = "subnet-0e3e2e731a8901f56"
+}
+
+# Replace this with another subnet in a *different AZ*
+data "aws_subnet" "subnet2" {
+  id = "subnet-0771ca28f3d9ae7f4"  # Replace with real working subnet ID from another AZ
+}
+
+resource "aws_eks_cluster" "eks" {
+  name     = "eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      data.aws_subnet.subnet1.id,
+      data.aws_subnet.subnet2.id
+    ]
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
 }
 
 resource "aws_iam_role" "eks_cluster_role" {
@@ -16,18 +40,18 @@ resource "aws_iam_role" "eks_cluster_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
+      Action = "sts:AssumeRole",
       Effect = "Allow",
       Principal = {
         Service = "eks.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
+      }
     }]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
 }
 
 resource "aws_iam_role" "eks_node_group_role" {
@@ -51,54 +75,35 @@ resource "aws_iam_role_policy_attachment" "node_group_policies" {
     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   ])
-
+  policy_arn = each.key
   role       = aws_iam_role.eks_node_group_role.name
-  policy_arn = each.value
 }
 
 resource "aws_security_group" "eks_cluster_sg" {
   name        = "eks-cluster-sg"
   description = "EKS Cluster security group"
-  vpc_id      = data.aws_vpc.selected.id
+  vpc_id      = data.aws_vpc.eks_vpc.id
 }
 
 resource "aws_security_group" "eks_node_sg" {
   name        = "eks-node-sg"
   description = "EKS Worker Node security group"
-  vpc_id      = data.aws_vpc.selected.id
+  vpc_id      = data.aws_vpc.eks_vpc.id
 }
 
-resource "aws_eks_cluster" "eks" {
-  name     = "eks-cluster"
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids         = [data.aws_subnet.selected.id]
-    security_group_ids = [aws_security_group.eks_cluster_sg.id]
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_policy
-  ]
-}
-
-resource "aws_eks_node_group" "eks_nodes" {
+resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.eks_node_group_role.arn
-  subnet_ids      = [data.aws_subnet.selected.id]
-
+  subnet_ids      = [
+    data.aws_subnet.subnet1.id,
+    data.aws_subnet.subnet2.id
+  ]
+  instance_types  = ["t2.medium"]
   scaling_config {
     desired_size = 2
     max_size     = 2
     min_size     = 1
-  }
-
-  instance_types = ["t2.medium"]
-
-  remote_access {
-    ec2_ssh_key               = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.eks_node_sg.id]
   }
 
   depends_on = [
